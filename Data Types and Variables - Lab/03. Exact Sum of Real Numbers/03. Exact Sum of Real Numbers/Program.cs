0cb4c2f32a81e5c9c3791519294a81f1338a2067
KNKostadinov21 <106849849+KNKostadinov21@users.noreply.github.com>
2023-01-18 19:37:57 +0200

    03. Exact Sum of Real Numbers

---
+++ Data Types and Variables - Lab/03. Exact Sum of Real Numbers/03. Exact Sum of Real Numbers/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _03._Exact_Sum_of_Real_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numbers = int.Parse(Console.ReadLine());
+            decimal totalSum = 0;
+
+            for (int i = 1; i <= numbers; i++)
+            {
+                decimal currentNumber = decimal.Parse(Console.ReadLine());
+                totalSum += currentNumber;
+            }
+
+            Console.WriteLine(totalSum);
+        }
+    }
+}
