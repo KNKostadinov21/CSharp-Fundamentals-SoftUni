a378d2b0ccb73284cddf1229079a9ac73dc5ff8f
KNKostadinov21 <106849849+KNKostadinov21@users.noreply.github.com>
2023-01-18 18:00:13 +0200

    07. Theatre Promotion

---
+++ Basic Syntax, Conditional Statements and Loops - Lab/07. Theatre Promotion/07. Theatre Promotion/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace _07._Theatre_Promotion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string day = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+
+            if (day == "Weekday")
+            {
+                if (age >= 0 && age <= 18)
+                {
+                    Console.WriteLine("12$");
+                }
+                else if (age > 18 && age <= 64)
+                {
+                    Console.WriteLine("18$");
+                }
+                else if (age > 64 && age <= 122)
+                {
+                    Console.WriteLine("12$");
+                }
+                else
+                {
+                    Console.WriteLine("Error!");
+                }
+            }
+
+            else if (day == "Weekend")
+            {
+                if (age >= 0 && age <= 18)
+                {
+                    Console.WriteLine("15$");
+                }
+                else if (age > 18 && age <= 64)
+                {
+                    Console.WriteLine("20$");
+                }
+                else if (age > 64 && age <= 122)
+                {
+                    Console.WriteLine("15$");
+                }
+                else
+                {
+                    Console.WriteLine("Error!");
+                }
+            }
+
+            else
+            {
+                if (age >= 0 && age <= 18)
+                {
+                    Console.WriteLine("5$");
+                }
+                else if (age > 18 && age <= 64)
+                {
+                    Console.WriteLine("12$");
+                }
+                else if (age > 64 && age <= 122)
+                {
+                    Console.WriteLine("10$");
+                }
+                else
+                {
+                    Console.WriteLine("Error!");
+                }
+            }
+        }
+    }
+}
