32944934074a88585ebea752b6389649f4180c85
KNKostadinov21 <106849849+KNKostadinov21@users.noreply.github.com>
2023-01-18 20:18:41 +0200

    05. Special Numbers

---
+++ Data Types and Variables - Lab/05. Special Numbers/05. Special Numbers/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _05._Special_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                int currentNumber = i;
+                int sum = 0;
+
+                while (currentNumber > 0)
+                {
+                    sum += currentNumber % 10;
+                    currentNumber /= 10;
+                }
+
+                if (sum == 5 || sum == 7 || sum == 11)
+                {
+                    Console.WriteLine($"{i} -> True");
+                }
+
+                else
+                {
+                    Console.WriteLine($"{i} -> False");
+                }
+            }
+        }
+    }
+}
