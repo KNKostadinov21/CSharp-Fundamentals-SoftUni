550f4de579a9975a24f72fa60c4c2878f9ca8361
KNKostadinov21 <106849849+KNKostadinov21@users.noreply.github.com>
2023-01-18 19:23:48 +0200

    01. Convert Meters to Kilometers

---
+++ Data Types and Variables - Lab/01. Convert Meters to Kilometers/01. Convert Meters to Kilometers/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _01._Convert_Meters_to_Kilometers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int meters = int.Parse(Console.ReadLine());
+            double kilometers = meters / 1000d;
+            Console.WriteLine($"{kilometers:F2}");  
+        }
+    }
+}
