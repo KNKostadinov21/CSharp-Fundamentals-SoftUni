0ba35215d8fb69b0abb05feec48011b75bc168e7
KNKostadinov21 <106849849+KNKostadinov21@users.noreply.github.com>
2023-01-19 20:12:49 +0200

    04. Reverse String

---
+++ Basic Syntax, Conditional Statements and Loops - More Exercise/04. Reverse String/04. Reverse String/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _04._Reverse_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string stringInput = Console.ReadLine();
+            string reversedString = null;
+
+            for(int i = stringInput.Length - 1; i >= 0; i--)
+            {
+                reversedString += stringInput[i];
+            }
+
+            Console.WriteLine(reversedString);
+        }
+    }
+}
