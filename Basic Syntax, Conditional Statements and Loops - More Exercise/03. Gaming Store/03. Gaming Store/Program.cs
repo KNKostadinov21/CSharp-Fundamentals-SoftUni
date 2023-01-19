82025ef2e23a77c4d65d83c8f1309352dcada8c4
KNKostadinov21 <106849849+KNKostadinov21@users.noreply.github.com>
2023-01-19 20:08:39 +0200

    03. Gaming Store

---
+++ Basic Syntax, Conditional Statements and Loops - More Exercise/03. Gaming Store/03. Gaming Store/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+
+namespace _03._Gaming_Store
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            double budgetCopy = budget;
+            string input = Console.ReadLine();
+
+            while(true)
+            {
+                if(input == "Game Time")
+                {
+                    break;
+                }
+
+                switch (input)
+                {
+                    case "OutFall 4":
+                        if(budget >= 39.99)
+                        {
+                            budget -= 39.99;
+                            Console.WriteLine("Bought OutFall 4");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Too Expensive");
+                        }
+                        break;
+                    case "CS: OG":
+                        if(budget >= 15.99)
+                        {
+                            budget -= 15.99;
+                            Console.WriteLine("Bought CS: OG");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Too Expensive");
+                        }
+                        break;
+                    case "Zplinter Zell":
+                        if(budget >= 19.99)
+                        {
+                            budget -= 19.99;
+                            Console.WriteLine("Bought Zplinter Zell");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Too Expensive");
+                        }
+                        break;
+                    case "Honored 2":
+                        if(budget >= 59.99)
+                        {
+                            budget -= 59.99;
+                            Console.WriteLine("Bought Honored 2");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Too Expensive");
+                        }
+                        break;
+                    case "RoverWatch":
+                        if (budget >= 29.99)
+                        {
+                            budget -= 29.99;
+                            Console.WriteLine("Bought RoverWatch");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Too Expensive");
+                        }
+                        break;
+                    case "RoverWatch Origins Edition":
+                        if(budget >= 39.99)
+                        {
+                            budget -= 39.99;
+                            Console.WriteLine("Bought RoverWatch Origins Edition");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Too Expensive");
+                        }
+                        break;
+                    default:
+                        Console.WriteLine("Not Found");
+                        break;
+                }
+
+                input = Console.ReadLine();
+            }
+
+            if(budget <= 0)
+            {
+                Console.WriteLine("Out of money!");
+            }
+            else
+            {
+                Console.WriteLine($"Total spent: ${(budgetCopy - budget):F2}. Remaining: ${budget:F2}");
+            }
+        }
+    }
+}
