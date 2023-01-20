6ee78d257cd74402a84bdc0f6ddf67ee20dc9b64
KNKostadinov21 <106849849+KNKostadinov21@users.noreply.github.com>
2023-01-20 18:55:06 +0200

    03. Elevator

---
+++ Data Types and Variables - Exercise/03. Elevator/03. Elevator/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _03._Elevator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int people = int.Parse(Console.ReadLine());
+            int elevatorCapacity = int.Parse(Console.ReadLine());
+
+            if (people % elevatorCapacity == 0)
+            {
+                Console.WriteLine(people / elevatorCapacity);
+            }
+
+            else
+            {
+                Console.WriteLine((people / elevatorCapacity) + 1);
+            }
+        }
+    }
+}
