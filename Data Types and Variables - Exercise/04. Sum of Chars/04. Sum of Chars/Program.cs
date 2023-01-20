c7c89418361f7987dfdf47fae55d9426950c06b1
KNKostadinov21 <106849849+KNKostadinov21@users.noreply.github.com>
2023-01-20 19:00:21 +0200

    04. Sum of Chars

---
+++ Data Types and Variables - Exercise/04. Sum of Chars/04. Sum of Chars/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _04._Sum_of_Chars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int sum = default;
+
+            for (int i = 1; i <= n; i++)
+            {
+                char letter = char.Parse(Console.ReadLine());
+
+                sum += (int)letter;
+            }
+
+            Console.WriteLine($"The sum equals: {sum}");
+        }
+    }
+}
