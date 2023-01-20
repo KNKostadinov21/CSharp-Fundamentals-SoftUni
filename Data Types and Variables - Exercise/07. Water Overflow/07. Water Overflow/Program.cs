bad0cee125e827da714e662a1d8a0156bdb9f3ac
KNKostadinov21 <106849849+KNKostadinov21@users.noreply.github.com>
2023-01-20 19:51:07 +0200

    07. Water Overflow

---
+++ Data Types and Variables - Exercise/07. Water Overflow/07. Water Overflow/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _07._Water_Overflow
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numbersLiters = int.Parse(Console.ReadLine());
+            int totalQuantity = 255;
+
+            for (int i = 1; i <= numbersLiters; i++)
+            {
+                int currentLiters = int.Parse(Console.ReadLine());
+
+                if (totalQuantity - currentLiters >= 0)
+                {
+                    totalQuantity -= currentLiters;
+                }
+
+                else
+                {
+                    Console.WriteLine("Insufficient capacity!");
+                }
+            }
+
+            Console.WriteLine(255 - totalQuantity);
+        }
+    }
+}
