b2ce142f92197d2a00d202e28686c8eef993767d
KNKostadinov21 <106849849+KNKostadinov21@users.noreply.github.com>
2023-01-20 19:59:31 +0200

    08. Beer Kegs

---
+++ Data Types and Variables - Exercise/08. Beer Kegs/08. Beer Kegs/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _08._Beer_Kegs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            double max = double.MinValue;
+            string currentKeg = string.Empty;
+
+            for (int i = 1; i <= n; i++)
+            {
+                string model = Console.ReadLine();
+                double radius = double.Parse(Console.ReadLine());
+                int height = int.Parse(Console.ReadLine());
+
+                double currentVolume = Math.PI * Math.Pow(radius, 2) * height;
+
+                if (max < currentVolume)
+                {
+                    max = currentVolume;
+                    currentKeg = model;
+                }
+            }
+
+            Console.WriteLine(currentKeg);
+        }
+    }
+}
