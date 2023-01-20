44260ff84c38ee9ab7fc8f65251ed001c4d16773
KNKostadinov21 <106849849+KNKostadinov21@users.noreply.github.com>
2023-01-20 22:02:00 +0200

    09. Spice Must Flow

---
+++ Data Types and Variables - Exercise/09. Spice Must Flow/09. Spice Must Flow/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _09._Spice_Must_Flow
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int yield = int.Parse(Console.ReadLine());
+            int daysOperated = 0;
+            int sum = 0;
+            int spicePerDay = 0;
+
+            while (yield >= 100)
+            {
+                daysOperated += 1;
+                spicePerDay = yield - 26;
+                sum += spicePerDay;
+                yield -= 10;
+            }
+
+            if (daysOperated > 0)
+            {
+                sum -= 26;
+            }
+
+            Console.WriteLine(daysOperated);
+            Console.WriteLine(sum);
+        }
+    }
+}
