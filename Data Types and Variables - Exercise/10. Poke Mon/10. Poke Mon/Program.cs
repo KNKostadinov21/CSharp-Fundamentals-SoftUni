9a6264703affa7ae2abcc5bec2ecab8c550c76c3
KNKostadinov21 <106849849+KNKostadinov21@users.noreply.github.com>
2023-01-20 22:15:14 +0200

    10. Poke Mon

---
+++ Data Types and Variables - Exercise/10. Poke Mon/10. Poke Mon/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _10._Poke_Mon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int pokePower = int.Parse(Console.ReadLine());
+            int pokePowerCopy = pokePower;
+            int distanceBetweenThePokeTargets = int.Parse(Console.ReadLine());
+            int exhaustionFactor = int.Parse(Console.ReadLine());
+            int targetsReachedCounter = 0;
+
+            while (pokePower >= distanceBetweenThePokeTargets)
+            {
+                pokePower -= distanceBetweenThePokeTargets;
+                targetsReachedCounter++;
+
+                if (pokePowerCopy * 0.5 == pokePower && exhaustionFactor != 0)
+                {
+                    pokePower /= exhaustionFactor;
+                }
+            }
+
+            Console.WriteLine(pokePower);
+            Console.WriteLine(targetsReachedCounter);
+        }
+    }
+}
