43753a04c7d465555111a740acbb66c7c3cf9b9e
KNKostadinov21 <106849849+KNKostadinov21@users.noreply.github.com>
2023-01-25 19:48:20 +0200

    02. Print Numbers in Reverse Order

---
+++ Arrays - Lab/02. Print Numbers in Reverse Order/02. Print Numbers in Reverse Order/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _02._Print_Numbers_in_Reverse_Order
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numbersOfArray = int.Parse(Console.ReadLine());
+
+            int[] arr = new int[numbersOfArray];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+
+            for (int i = numbersOfArray - 1; i >= 0; i--)
+            {
+                Console.Write(arr[i] + " ");
+            }
+        }
+    }
+}
