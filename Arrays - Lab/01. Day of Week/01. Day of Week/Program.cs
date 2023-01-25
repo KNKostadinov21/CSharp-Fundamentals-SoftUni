0e1aa3a9a3d477787226a852749b71e93e204b00
KNKostadinov21 <106849849+KNKostadinov21@users.noreply.github.com>
2023-01-25 19:12:53 +0200

    01. Day of Week

---
+++ Arrays - Lab/01. Day of Week/01. Day of Week/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _01._Day_of_Week
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] daysOfWeek = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+
+            int intAsDayOfWeek = int.Parse(Console.ReadLine());
+
+            if (intAsDayOfWeek >= 1 && intAsDayOfWeek <= 7)
+            {
+                Console.WriteLine(daysOfWeek[intAsDayOfWeek - 1]);
+            }
+            else
+            {
+                Console.WriteLine("Invalid day!");
+            }
+        }
+    }
+}
