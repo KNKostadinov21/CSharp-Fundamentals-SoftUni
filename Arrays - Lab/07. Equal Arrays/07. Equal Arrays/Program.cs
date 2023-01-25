2d0cae5df4e2af70aa4655771ddb242e9f9ea0f4
KNKostadinov21 <106849849+KNKostadinov21@users.noreply.github.com>
2023-01-25 20:49:42 +0200

    07. Equal Arrays

---
+++ Arrays - Lab/07. Equal Arrays/07. Equal Arrays/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+
+namespace _07._Equal_Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] firstArr = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            int[] secondArr = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int sum = 0;
+            bool areSame = true;
+
+            foreach (var currentNum in firstArr)
+            {
+                sum += currentNum;
+            }
+
+            for (int i = 0; i < secondArr.Length; i++)
+            {
+                if (firstArr[i] != secondArr[i])
+                {
+                    Console.WriteLine($"Arrays are not identical. Found difference at {i} index");
+                    areSame = false;
+                    break;
+                }
+            }
+
+            if (areSame)
+            {
+                Console.WriteLine($"Arrays are identical. Sum: {sum}");
+            }
+        }
+    }
+}
