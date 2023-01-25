c3f3b3285971be6575a8c30c12b60f9ac17a0df5
KNKostadinov21 <106849849+KNKostadinov21@users.noreply.github.com>
2023-01-25 21:07:32 +0200

    08. Condense Array to Number

---
+++ Arrays - Lab/08. Condense Array to Number/08. Condense Array to Number/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace _08._Condense_Array_to_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                for (int j = 0; j < arr.Length - 1 - i; j++)
+                {
+                    arr[j] = arr[j] + arr[j + 1];
+                }
+            }
+
+            Console.WriteLine(arr[0]);
+        }
+    }
+}
