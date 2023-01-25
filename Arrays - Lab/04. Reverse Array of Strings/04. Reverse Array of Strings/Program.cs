ac97d923b99b8a714d9228c0cf197f82cb93ccb6
KNKostadinov21 <106849849+KNKostadinov21@users.noreply.github.com>
2023-01-25 20:27:57 +0200

    04. Reverse Array of Strings

---
+++ Arrays - Lab/04. Reverse Array of Strings/04. Reverse Array of Strings/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace _04._Reverse_Array_of_Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arr = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            Array.Reverse(arr);
+
+            Console.WriteLine(string.Join(" ", arr));
+        }
+    }
+}
