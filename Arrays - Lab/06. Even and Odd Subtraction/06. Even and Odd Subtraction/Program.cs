7ae70ce7ed7c707ee94af015e843164a3d7d7754
KNKostadinov21 <106849849+KNKostadinov21@users.noreply.github.com>
2023-01-25 20:39:07 +0200

    06. Even and Odd Subtraction

---
+++ Arrays - Lab/06. Even and Odd Subtraction/06. Even and Odd Subtraction/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace _06._Even_and_Odd_Subtraction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int sumEven = 0;
+            int sumOdd = 0;
+
+            foreach (var currentNumber in arr)
+            {
+                if (currentNumber % 2 == 0)
+                {
+                    sumEven += currentNumber;
+                }
+                else
+                {
+                    sumOdd += currentNumber;
+                }
+            }
+
+            Console.WriteLine(sumEven - sumOdd);
+        }
+    }
+}
