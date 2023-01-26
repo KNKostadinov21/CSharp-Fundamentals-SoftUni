cd72818264d84ee8ef299faff292e0b64ad5eb60
KNKostadinov21 <106849849+KNKostadinov21@users.noreply.github.com>
2023-01-26 17:10:57 +0200

    02. Common Elements

---
+++ Arrays - Exercise/02. Common Elements/02. Common Elements/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace _02._Common_Elements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] firstArr = Console.ReadLine()
+                .Split()
+                .ToArray();
+            string[] secondArr = Console.ReadLine()
+                .Split()
+                .ToArray();
+
+            foreach (var item in firstArr)
+            {
+                for (int i = 0; i < secondArr.Length; i++)
+                {
+                    if (item == secondArr[i])
+                    {
+                        Console.Write(item + " ");
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
