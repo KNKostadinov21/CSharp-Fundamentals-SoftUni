c06ccb750cd0dc0b0eab2c7d87590db80f71c4c0
KNKostadinov21 <106849849+KNKostadinov21@users.noreply.github.com>
2023-01-26 16:54:22 +0200

    01. Train

---
+++ Arrays - Exercise/01. Train/01. Train/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _01._Train
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numbers = int.Parse(Console.ReadLine());
+            int[] arr = new int[numbers];
+
+            int totalSum = 0;
+            for (int i = 0; i < numbers; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+                totalSum += arr[i];
+                Console.Write(arr[i] + " ");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine(totalSum);
+        }
+    }
+}
