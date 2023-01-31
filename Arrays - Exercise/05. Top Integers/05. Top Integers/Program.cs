5a9782b42c315d4c35a6820218d980e2c37bc339
KNKostadinov21 <106849849+KNKostadinov21@users.noreply.github.com>
2023-01-31 16:31:37 +0200

    05. Top Integers

---
+++ Arrays - Exercise/05. Top Integers/05. Top Integers/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace _05._Top_Integers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                bool isBigger = true;
+                int number = arr[i];
+
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[i] <= arr[j])
+                    {
+                        isBigger = false;
+                        break;
+                    }
+                }
+
+                if (isBigger)
+                {
+                    Console.Write(number + " ");
+                }
+            }
+        }
+    }
+}
