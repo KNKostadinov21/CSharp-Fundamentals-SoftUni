9dcdafab1fb3c0d46df3dc09cac6db9c47a5c09e
KNKostadinov21 <106849849+KNKostadinov21@users.noreply.github.com>
2023-01-31 16:38:26 +0200

    06. Equal Sum

---
+++ Arrays - Exercise/06. Equal Sum/06. Equal Sum/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+namespace _06._Equal_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int leftSum = 0;
+                int rightSum = 0;
+
+                for (int j = i - 1; j >= 0; j--)
+                {
+                    leftSum += arr[j];
+                }
+
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    rightSum += arr[j];
+                }
+
+                if (leftSum == rightSum)
+                {
+                    Console.WriteLine(i);
+                    return;
+                }
+            }
+
+            Console.WriteLine("no");
+        }
+    }
+}
