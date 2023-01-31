f26914e809796c7cb95c2ce18bf2ac56346854a5
KNKostadinov21 <106849849+KNKostadinov21@users.noreply.github.com>
2023-01-31 16:59:00 +0200

    07. Max Sequence of Equal Elements

---
+++ Arrays - Exercise/07. Max Sequence of Equal Elements/07. Max Sequence of Equal Elements/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+namespace _07._Max_Sequence_of_Equal_Elements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int longestSequenceStartIndex = 0;
+            int longestSequenceLength = 0;
+
+            for (int i = 1; i < arr.Length; i++)
+            {
+                int currentSequenceStartIndex = i - 1;
+                int currentSequenceLength = 1;
+
+                while (i < arr.Length && arr[i] == arr[i - 1])
+                {
+                    currentSequenceLength++;
+                    i++;
+                }
+
+                if (currentSequenceLength > longestSequenceLength)
+                {
+                    longestSequenceLength = currentSequenceLength;
+                    longestSequenceStartIndex = currentSequenceStartIndex;
+                }
+            }
+
+            for (int i = longestSequenceStartIndex; i < longestSequenceStartIndex + longestSequenceLength; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+        }
+    }
+}
