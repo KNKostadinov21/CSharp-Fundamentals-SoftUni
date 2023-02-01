faf021d173a2eab9848a68d7dd5b74eabe20e850
KNKostadinov21 <106849849+KNKostadinov21@users.noreply.github.com>
2023-02-01 19:37:32 +0200

    04. Printing Triangle

---
+++ Methods - Lab/04. Printing Triangle/04. Printing Triangle/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _04._Printing_Triangle
+{
+    internal class Program
+    {
+        static void PrintLine(int start, int end)
+        {
+            for (int i = start; i <= end; i++)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+        }
+
+        static void Main(string[] args)
+        {
+            int end = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= end; i++)
+            {
+                PrintLine(1, i);
+            }
+            for (int i = end - 1; i >= 1; i--)
+            {
+                PrintLine(1, i);
+            }
+        }
+    }
+}
