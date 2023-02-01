c9ae31edc2d7bc8887d8e12c8c3c5cb190f3476a
KNKostadinov21 <106849849+KNKostadinov21@users.noreply.github.com>
2023-02-01 19:57:25 +0200

    07. Repeat String

---
+++ Methods - Lab/07. Repeat String/07. Repeat String/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _07._Repeat_String
+{
+    internal class Program
+    {
+        static string StringRepeats(string str, int repeats)
+        {
+            string newString = string.Empty;
+            for (int i = 1; i <= repeats; i++)
+            {
+                newString += str;
+            }
+
+            return newString;
+        }
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            int repeats = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(StringRepeats(str, repeats));
+        }
+    }
+}
