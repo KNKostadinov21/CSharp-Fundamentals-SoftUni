380f2c92a6601bcbf234c282c08e8cd7cfc3ea3e
KNKostadinov21 <106849849+KNKostadinov21@users.noreply.github.com>
2023-02-01 19:24:23 +0200

    03. Calculations

---
+++ Methods - Lab/03. Calculations/03. Calculations/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace _03._Calculations
+{
+    internal class Program
+    {
+        static void Add(int firstNum, int secondNum)
+        {
+            Console.WriteLine(firstNum + secondNum);
+        }
+
+        static void Subtract(int firstNum, int secondNum)
+        {
+            Console.WriteLine(firstNum - secondNum);
+        }
+
+        static void Multiply(int firstNum, int secondNum)
+        {
+            Console.WriteLine(firstNum * secondNum);
+        }
+
+        static void Divide(int firstNum, int secondNum)
+        {
+            Console.WriteLine(firstNum / secondNum);
+        }
+
+        static void Main(string[] args)
+        {
+            string operation = Console.ReadLine();
+            int firstNum = int.Parse(Console.ReadLine());
+            int secondNum = int.Parse(Console.ReadLine());
+
+            switch (operation)
+            {
+                case "add":
+                    Add(firstNum, secondNum);
+                    break;
+                case "subtract":
+                    Subtract(firstNum, secondNum);
+                    break;
+                case "multiply":
+                    Multiply(firstNum, secondNum);
+                    break;
+                case "divide":
+                    Divide(firstNum, secondNum);
+                    break;
+            }
+        }
+    }
+}
