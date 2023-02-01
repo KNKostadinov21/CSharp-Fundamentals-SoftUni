a8b2a27bbbd9c374d6459f919d432a042aa8e54f
KNKostadinov21 <106849849+KNKostadinov21@users.noreply.github.com>
2023-02-01 19:45:17 +0200

    05. Orders

--- Methods - Lab/05. Orders/05. Orders/Program.cs
+++ Methods - Lab/05. Orders/05. Orders/Program.cs
@@ -2,11 +2,33 @@
 
 namespace _05._Orders
 {
-    class Program
+    internal class Program
     {
+        static void Order(string product, int quantity)
+        {
+            double pricePerOne = 0;
+
+            switch (product)
+            {
+                case "coffee":
+                    pricePerOne = 1.50; break;
+                case "water":
+                    pricePerOne = 1.00; break;
+                case "coke":
+                    pricePerOne = 1.40; break;
+                case "snacks":
+                    pricePerOne = 2.00; break;
+            }
+
+            Console.WriteLine($"{(quantity * pricePerOne):F2}");
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string product = Console.ReadLine();
+            int quantity = int.Parse(Console.ReadLine());
+
+            Order(product, quantity);
         }
     }
 }
