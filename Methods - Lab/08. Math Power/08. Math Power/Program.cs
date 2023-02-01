88b1f6ef5becb931a5e223e0c0aa5a97bfd56d40
KNKostadinov21 <106849849+KNKostadinov21@users.noreply.github.com>
2023-02-01 20:09:19 +0200

    08. Math Power

---
+++ Methods - Lab/08. Math Power/08. Math Power/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _08._Math_Power
+{
+    internal class Program
+    {
+        static double MathPow(double number, int power)
+        {
+            double result = Math.Pow(number, power);
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            double number = double.Parse(Console.ReadLine());
+            int power = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(MathPow(number, power));
+        }
+    }
+}
