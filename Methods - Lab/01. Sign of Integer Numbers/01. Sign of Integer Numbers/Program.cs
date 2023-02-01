e3aec16012b02eb181f319c38264e9caea7e4e87
KNKostadinov21 <106849849+KNKostadinov21@users.noreply.github.com>
2023-02-01 19:06:33 +0200

    01. Sign of Integer Numbers

---
+++ Methods - Lab/01. Sign of Integer Numbers/01. Sign of Integer Numbers/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _01._Sign_of_Integer_Numbers
+{
+    internal class Program
+    {
+        static void signOfNumber(int number)
+        {
+            if (number == 0)
+            {
+                Console.WriteLine($"The number {number} is zero.");
+            }
+            else if (number < 0)
+            {
+                Console.WriteLine($"The number {number} is negative.");
+            }
+            else
+            {
+                Console.WriteLine($"The number {number} is positive.");
+            }
+        }
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            signOfNumber(number);
+        }
+    }
+}
