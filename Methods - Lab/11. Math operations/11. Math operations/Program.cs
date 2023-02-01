ec2084773095e6ad91292531766c4c6508ba87bf
KNKostadinov21 <106849849+KNKostadinov21@users.noreply.github.com>
2023-02-01 20:31:47 +0200

    11. Math operations

---
+++ Methods - Lab/11. Math operations/11. Math operations/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _11._Math_operations
+{
+    internal class Program
+    {
+        static double Calculation(double firstNum, string operations, double secondNum)
+        {
+            double result = 0;
+
+            switch (operations)
+            {
+                case "+":
+                    result = firstNum + secondNum; break;
+                case "-":
+                    result = firstNum - secondNum; break;
+                case "*":
+                    result = firstNum * secondNum; break;
+                case "/":
+                    result = firstNum / secondNum; break;
+            }
+
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            double firstNum = double.Parse(Console.ReadLine());
+            string operation = Console.ReadLine();
+            double secondNum = double.Parse(Console.ReadLine());
+
+            Console.WriteLine(Calculation(firstNum, operation, secondNum));
+        }
+    }
+}
