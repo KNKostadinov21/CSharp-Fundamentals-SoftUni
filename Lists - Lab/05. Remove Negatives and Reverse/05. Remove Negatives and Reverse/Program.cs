aac1571c37414ff87fead5025e7cd73b0cef64b9
KNKostadinov21 <106849849+KNKostadinov21@users.noreply.github.com>
2023-02-18 23:43:02 +0200

    05. Remove Negatives and Reverse

---
+++ Lists - Lab/05. Remove Negatives and Reverse/05. Remove Negatives and Reverse/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _05._Remove_Negatives_and_Reverse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            numbers.RemoveAll(x => x < 0);
+
+            if (numbers.Count == 0)
+            {
+                Console.WriteLine("empty");
+            }
+            else
+            {
+                numbers.Reverse();
+                Console.WriteLine(string.Join(" ", numbers));
+            }
+        }
+    }
+}
