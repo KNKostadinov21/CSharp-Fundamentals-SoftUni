817e339aab51031300ddaf47d4cabfca79df180e
KNKostadinov21 <106849849+KNKostadinov21@users.noreply.github.com>
2023-02-18 23:11:50 +0200

    02. Gauss' Trick

---
+++ Lists - Lab/02. Gauss' Trick/02. Gauss' Trick/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _02._Gauss__Trick
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            List<int> result = new List<int>();
+
+            for (int i = 0; i < numbers.Count / 2; i++)
+            {
+                int currentSum = numbers[i] + numbers[numbers.Count - 1 - i];
+                result.Add(currentSum);
+            }
+
+            if (numbers.Count % 2 == 1)
+            {
+                result.Add(numbers[numbers.Count / 2]);
+            }
+
+            Console.WriteLine(string.Join(" ", result));
+        }
+    }
+}
