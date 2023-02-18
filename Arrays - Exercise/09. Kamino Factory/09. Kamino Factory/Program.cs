ee4c6eed11c6ae2ddebc237f624d5ab5842e11f1
KNKostadinov21 <106849849+KNKostadinov21@users.noreply.github.com>
2023-02-18 21:24:55 +0200

    09. Kamino Factory

---
+++ Arrays - Exercise/09. Kamino Factory/09. Kamino Factory/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Linq;
+
+namespace _09._Kamino_Factory
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int dnaLength = int.Parse(Console.ReadLine());
+
+            int[] currentLongestSequence = new int[dnaLength];
+            int currentSequenceLength = 0;
+            int currentStartingIndex = 0;
+            int currentSum = 0;
+            int dnaNumber = 0;
+
+            int counter = 0;
+            while (true)
+            {
+                counter++;
+                string input = Console.ReadLine();
+
+                if (input == "Clone them!")
+                {
+                    break;
+                }
+
+                int[] arr = input
+                    .Split("!", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                int length = 0;
+
+                foreach (var item in arr)
+                {
+                    if (item == 1)
+                    {
+                        length++;
+                    }
+                    else if (item == 0 && length > 0)
+                    {
+                        break;
+                    }
+                }
+
+                int startingIndex = Array.IndexOf(arr, 1);
+                int sum = arr.Sum();
+
+                if (length > currentSequenceLength
+                    || length == currentSequenceLength
+                    && currentStartingIndex > startingIndex
+                    || length == currentStartingIndex
+                    && sum > currentSum)
+                {
+                    currentLongestSequence = arr;
+                    currentSequenceLength = length;
+                    currentStartingIndex = startingIndex;
+                    currentSum = sum;
+                    dnaNumber = counter;
+                }
+            }
+
+            Console.WriteLine($"Best DNA sample {dnaNumber} with sum: {currentSum}.");
+            Console.WriteLine(string.Join(' ', currentLongestSequence));
+        }
+    }
+}
