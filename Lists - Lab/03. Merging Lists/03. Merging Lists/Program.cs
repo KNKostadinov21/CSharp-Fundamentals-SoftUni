dfac6785830392f818e424479128685f0b472b72
KNKostadinov21 <106849849+KNKostadinov21@users.noreply.github.com>
2023-02-18 23:27:14 +0200

    03. Merging Lists

---
+++ Lists - Lab/03. Merging Lists/03. Merging Lists/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+namespace _03._Merging_Lists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> leftList = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            List<int> rightList = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            List<int> result = new List<int>();
+
+            int minValue = Math.Min(leftList.Count, rightList.Count);
+
+            for (int i = 0; i < minValue; i++)
+            {
+                result.Add(leftList[i]);
+                result.Add(rightList[i]);
+            }
+
+            if (leftList.Count > rightList.Count)
+            {
+                for (int i = minValue; i < leftList.Count; i++)
+                {
+                    result.Add(leftList[i]);
+                }
+            }
+            else if (rightList.Count > leftList.Count)
+            {
+                for (int i = minValue; i < rightList.Count; i++)
+                {
+                    result.Add(rightList[i]);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", result));
+        }
+    }
+}
