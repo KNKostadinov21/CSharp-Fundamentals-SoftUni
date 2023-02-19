80b5202577b0e1d069879003258a976d72b0a78a
KNKostadinov21 <106849849+KNKostadinov21@users.noreply.github.com>
2023-02-19 11:44:45 +0200

    03. Phone Shop

---
+++ Programming Fundamentals Mid Exam - 19 February 2023/03. Phone Shop/03. Phone Shop/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _03._Phone_Shop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> phones = Console.ReadLine()
+                .Split(", ")
+                .ToList();
+
+            while (true)
+            {
+                string command = Console.ReadLine();
+                if (command == "End")
+                {
+                    break;
+                }
+
+                string[] commandInfo = command.Split(" - ");
+
+                if (commandInfo[0] == "Add")
+                {
+                    if (!phones.Contains(commandInfo[1]))
+                    {
+                        phones.Add(commandInfo[1]);
+                    }
+                }
+                else if (commandInfo[0] == "Remove")
+                {
+                    if (phones.Contains(commandInfo[1]))
+                    {
+                        phones.Remove(commandInfo[1]);
+                    }
+                }
+                else if (commandInfo[0] == "Bonus phone")
+                {
+                    string[] bonusParts = commandInfo[1].Split(":");
+                    string oldPhone = bonusParts[0];
+                    string newPhone = bonusParts[1];
+                    int index = phones.IndexOf(oldPhone);
+                    if (index != -1)
+                    {
+                        phones.Insert(index + 1, newPhone);
+                    }
+                }
+                else if (commandInfo[0] == "Last")
+                {
+                    if (phones.Contains(commandInfo[1]))
+                    {
+                        phones.Remove(commandInfo[1]);
+                        phones.Add(commandInfo[1]);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid command.");
+                }
+            }
+
+            Console.WriteLine(string.Join(", ", phones));
+        }
+    }
+}
