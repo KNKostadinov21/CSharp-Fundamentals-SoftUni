706f774a4cfb1d9664b63131a9557b36d73a02e6
KNKostadinov21 <106849849+KNKostadinov21@users.noreply.github.com>
2023-02-22 23:54:47 +0200

    03. Songs

---
+++ Objects and Classes - Lab/03. Songs/03. Songs/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _03._Songs
+{
+    public class Song
+    {
+        public string TypeList { get; set; }
+
+        public string Name { get; set; }
+
+        public string Time { get; set; }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+
+            List<Song> songs = new List<Song>();
+
+            for (int i = 1; i <= N; i++)
+            {
+                string[] input = Console.ReadLine().
+                    Split('_').
+                    ToArray();
+
+                string typeList = input[0];
+                string songName = input[1];
+                string duration = input[2];
+
+                Song song = new Song();
+                song.TypeList = typeList;
+                song.Name = songName;
+                song.Time = duration;
+                songs.Add(song);
+            }
+
+            string searchForTypeList = Console.ReadLine();
+
+            if (searchForTypeList == "all")
+            {
+                foreach (Song song in songs)
+                {
+                    Console.WriteLine(song.Name);
+                }
+            }
+            else
+            {
+                foreach (Song song in songs)
+                {
+                    if (song.TypeList == searchForTypeList)
+                    {
+                        Console.WriteLine(song.Name);
+                    }
+                }
+            }
+        }
+    }
+}
