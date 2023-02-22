1bcd5770bd0e63622faae56bda6a2df76c828e96
KNKostadinov21 <106849849+KNKostadinov21@users.noreply.github.com>
2023-02-22 19:30:05 +0200

    02. Big Factorial

---
+++ Objects and Classes - Lab/02. Big Factorial/02. Big Factorial/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Numerics;
+
+namespace _02._Big_Factorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int.TryParse(Console.ReadLine(), out int number);
+            BigInteger factorial = 1;
+
+            for (int i = 2; i <= number; i++)
+            {
+                factorial *= i;
+            }
+
+            Console.WriteLine(factorial);
+        }
+    }
+}
