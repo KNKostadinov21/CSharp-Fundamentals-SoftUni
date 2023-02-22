ccc51df5219f50e61660c01e84271f7b137b68ca
KNKostadinov21 <106849849+KNKostadinov21@users.noreply.github.com>
2023-02-22 19:19:58 +0200

    01. Randomize Words

---
+++ Objects and Classes - Lab/01. Randomize Words/01. Randomize Words/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace _01._Randomize_Words
+{
+    internal class Program
+
+    {
+        static void Main(string[] args)
+        {
+
+            string[] words = Console.ReadLine()
+                .Split()
+                .ToArray();
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                Random random = new Random();
+                int randomIndex = random.Next(0, words.Length);
+
+                string currentWord = words[i];
+                string wordToSwap = words[randomIndex];
+
+                words[i] = wordToSwap;
+                words[randomIndex] = currentWord;
+            }
+
+            Console.WriteLine(string.Join(Environment.NewLine, words));
+        }
+    }
+}
