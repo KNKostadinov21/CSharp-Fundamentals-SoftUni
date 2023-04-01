60dfc555c6f492ebad0f92be830c80d3476835f3
KNKostadinov21 <106849849+KNKostadinov21@users.noreply.github.com>
2023-04-01 18:04:53 +0300

    04. Caesar Cipher

---
+++ Text Processing - Exercise/04. Caesar Cipher/04. Caesar Cipher/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Text;
+
+internal class Program
+{
+    static void Main()
+    {
+        string input = Console.ReadLine();
+        StringBuilder result = new StringBuilder();
+
+        foreach (char c in input)
+            result.Append((char)(c + 3));
+        Console.WriteLine(result);
+    }
+}
