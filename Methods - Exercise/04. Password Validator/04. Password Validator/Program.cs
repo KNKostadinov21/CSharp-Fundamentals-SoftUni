de600f33ff30f863f58befb928da08950adff2c0
KNKostadinov21 <106849849+KNKostadinov21@users.noreply.github.com>
2023-04-01 15:48:50 +0300

    04. Password Validator

---
+++ Methods - Exercise/04. Password Validator/04. Password Validator/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace _04._Password_Validator
+{
+    internal class Program
+    {
+        static bool IsBetweenSixAndTenCharacters(string pass)
+        {
+            if (pass.Length >= 6 && pass.Length <= 10) return true;
+            Console.WriteLine("Password must be between 6 and 10 characters");
+            return false;
+        }
+
+        static bool ConsistsOnlyOfLettersAndDigits(string pass)
+        {
+            for (int i = 0; i < pass.Length; i++)
+            {
+                if (pass[i] > 47 && pass[i] < 58 || pass[i] > 64 && pass[i] < 91 || pass[i] > 96 && pass[i] < 123) continue;
+                Console.WriteLine("Password must consist only of letters and digits");
+                return false;
+            }
+            return true;
+        }
+
+        static bool HasAtLeastTwoDigits(string pass)
+        {
+            int digitsCount = 0;
+            for (int i = 0; i < pass.Length; i++)
+                if (pass[i] > 47 && pass[i] < 58) digitsCount++;
+            if (digitsCount >= 2) return true;
+            Console.WriteLine("Password must have at least 2 digits");
+            return false;
+        }
+
+        static void Main()
+        {
+            string input = Console.ReadLine();
+            bool passwordIsValid = true;
+            if (!IsBetweenSixAndTenCharacters(input)) passwordIsValid = false;
+            if (!ConsistsOnlyOfLettersAndDigits(input)) passwordIsValid = false;
+            if (!HasAtLeastTwoDigits(input)) passwordIsValid = false;
+
+            if (passwordIsValid) Console.WriteLine("Password is valid");
+        }
+    }
+}
