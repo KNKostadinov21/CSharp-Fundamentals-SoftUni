979ac134e02663e156c08cf83c12dc8d38409649
KNKostadinov21 <106849849+KNKostadinov21@users.noreply.github.com>
2023-04-01 16:31:52 +0300

    05. Courses

---
+++ Associative Arrays - Exercise/05. Courses/05. Courses/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+internal class Program
+{
+    static void Main()
+    {
+        Dictionary<string, List<string>> courses = new Dictionary<string, List<string>>();
+        string input;
+        while ((input = Console.ReadLine()) != "end")
+        {
+            string[] commands = input.Split(new string[] { " : " }, StringSplitOptions.None);
+            string course = commands[0];
+            string student = commands[1];
+            if (!courses.ContainsKey(course))
+                courses.Add(course, new List<string>());
+            courses[course].Add(student);
+        }
+
+        foreach (KeyValuePair<string, List<string>> course in courses)
+        {
+            Console.WriteLine($"{course.Key}: {course.Value.Count}");
+            course.Value.ForEach(x => Console.WriteLine($"-- {x}"));
+        }
+    }
+}
