8c0f84ebcd2492f754af842fe2130782113d2a77
KNKostadinov21 <106849849+KNKostadinov21@users.noreply.github.com>
2023-04-01 18:04:14 +0300

    03. Extract File

---
+++ Text Processing - Exercise/03. Extract File/03. Extract File/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+internal class Program
+{
+    static void Main()
+    {
+        string[] input = Console.ReadLine().Split(@"\");
+        string[] tokens = input[input.Length - 1].Split('.');
+        string fileName = tokens[0];
+        string fileExtension = tokens[1];
+        Console.WriteLine($"File name: {fileName}");
+        Console.WriteLine($"File extension: {fileExtension}");
+    }
+}
