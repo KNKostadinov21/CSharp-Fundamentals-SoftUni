7ca5b7e8aea083b9c1ff13c77191527e86f16761
KNKostadinov21 <106849849+KNKostadinov21@users.noreply.github.com>
2023-04-01 16:34:45 +0300

    07. Company Users

---
+++ Associative Arrays - Exercise/07. Company Users/07. Company Users/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+internal class Program
+{
+    static void Main()
+    {
+        Dictionary<string, List<string>> companyData = new Dictionary<string, List<string>>();
+        string input;
+        while ((input = Console.ReadLine()) != "End")
+        {
+            string[] commands = input.Split(new string[] { " -> " }, StringSplitOptions.None);
+            string company = commands[0];
+            string employee = commands[1];
+            if (!companyData.ContainsKey(company))
+                companyData.Add(company, new List<string>());
+            if (!companyData[company].Contains(employee))
+                companyData[company].Add(employee);
+        }
+
+        foreach (KeyValuePair<string, List<string>> company in companyData)
+        {
+            Console.WriteLine(company.Key);
+            foreach (string employee in company.Value)
+            {
+                Console.WriteLine($"-- {employee}");
+            }
+        }
+    }
+}
