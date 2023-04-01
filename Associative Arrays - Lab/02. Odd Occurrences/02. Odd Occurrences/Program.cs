1bb4329f980050d856d652424cf17205fc817016
KNKostadinov21 <106849849+KNKostadinov21@users.noreply.github.com>
2023-04-01 16:18:54 +0300

    02. Odd Occurrences

---
+++ Associative Arrays - Lab/02. Odd Occurrences/02. Odd Occurrences/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _02._Odd_Occurrences
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Dictionary<string, int> wordsCounts = new Dictionary<string, int>();
+            string[] input = Console.ReadLine().ToLower().Split();
+            foreach (string word in input)
+            {
+                if (!wordsCounts.ContainsKey(word))
+                    wordsCounts.Add(word, 0);
+                wordsCounts[word]++;
+            }
+            foreach (KeyValuePair<string, int> word in wordsCounts)
+            {
+                if (word.Value % 2 == 1)
+                    Console.Write(word.Key + " ");
+            }
+        }
+    }
+}
