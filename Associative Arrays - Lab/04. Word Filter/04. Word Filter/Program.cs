e8a73228c08dcdfa83fc8ebaf5517c1a8d42b22f
KNKostadinov21 <106849849+KNKostadinov21@users.noreply.github.com>
2023-04-01 16:21:29 +0300

    04. Word Filter

---
+++ Associative Arrays - Lab/04. Word Filter/04. Word Filter/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace _04._Word_Filter
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string[] words = Console.ReadLine().Split().Where(x => x.Length % 2 == 0).ToArray();
+            foreach (string word in words)
+                Console.WriteLine(word);
+        }
+    }
+}
