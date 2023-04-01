034d70f3b18cb26d11a553ec885b7f39c4ed34e9
KNKostadinov21 <106849849+KNKostadinov21@users.noreply.github.com>
2023-04-01 15:39:55 +0300

    01. Smallest of Three Numbers

---
+++ Methods - Exercise/01. Smallest of Three Numbers/01. Smallest of Three Numbers/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void PrintSmallestNumber(int a, int b, int c)
+    {
+        int smallest = Math.Min(a, b);
+        smallest = Math.Min(smallest, c);
+        Console.WriteLine(smallest);
+    }
+
+    static void Main()
+    {       
+        (int one, int two, int three) nums = (0, 0, 0);
+        nums.one = int.Parse(Console.ReadLine());
+        nums.two = int.Parse(Console.ReadLine());
+        nums.three = int.Parse(Console.ReadLine());
+        PrintSmallestNumber(nums.one, nums.two, nums.three);
+    }
+}
