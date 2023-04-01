60c8f21dd9e74c91e470d89ab163f14a2a7fc1af
KNKostadinov21 <106849849+KNKostadinov21@users.noreply.github.com>
2023-04-01 16:07:26 +0300

    05. Bomb Numbers

---
+++ Lists - Exercise/05. Bomb Numbers/05. Bomb Numbers/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05._Bomb_Numbers
+{
+    internal class Program
+    {
+        static (List<int>, bool) Detonation(List<int> list, int bombNumber, int power)
+        {
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (list[i] == bombNumber)
+                {
+                    int start = i - power;
+                    int end = i + power;
+                    if (start < 0) start = 0;
+                    if (end >= list.Count) end = list.Count - 1;
+                    list.RemoveRange(start, end - start + 1);
+                    return (list, true);
+                }
+            }
+            return (list, false);
+        }
+
+        static void Main()
+        {
+            List<int> list = Console.ReadLine().Split().Select(int.Parse).ToList();
+            int[] numberAndPower = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int bombNumber = numberAndPower[0];
+            int power = numberAndPower[1];
+            bool hasDetonated = true;
+            while (hasDetonated)
+            {
+                (list, hasDetonated) = Detonation(list, bombNumber, power);
+            }
+            Console.WriteLine(list.Sum());
+        }
+    }
+}
