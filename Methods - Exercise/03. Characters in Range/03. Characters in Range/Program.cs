563496e6fd6e37ef66b5f999b021107d51289b24
KNKostadinov21 <106849849+KNKostadinov21@users.noreply.github.com>
2023-04-01 15:47:18 +0300

    03. Characters in Range

---
+++ Methods - Exercise/03. Characters in Range/03. Characters in Range/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace _03._Characters_in_Range
+{
+    internal class Program
+    {
+        static void PrintAllCharsInBetween(char first, char second)
+        {
+            for (int i = first + 1; i < second; i++)
+            {
+                Console.Write((char)i + " ");
+            }
+        }
+
+        static void Main()
+        {
+            char symbol1 = char.Parse(Console.ReadLine());
+            char symbol2 = char.Parse(Console.ReadLine());
+            if (symbol1 < symbol2) PrintAllCharsInBetween(symbol1, symbol2);
+            else PrintAllCharsInBetween(symbol2, symbol1);
+        }
+    }
+}
