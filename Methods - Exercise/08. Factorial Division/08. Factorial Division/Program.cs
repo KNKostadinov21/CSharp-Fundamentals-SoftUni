b3cec4e960881a6b9eaf6f757b6ef3e5b8acb7c6
KNKostadinov21 <106849849+KNKostadinov21@users.noreply.github.com>
2023-04-01 15:57:07 +0300

    08. Factorial Division

---
+++ Methods - Exercise/08. Factorial Division/08. Factorial Division/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _08._Factorial_Division
+{
+    internal class Program
+    {
+        static decimal GetFactorial(decimal n)
+        {
+            decimal result = 1m;
+            for (int i = 1; i <= n; i++) result *= i;
+            return result;
+        }
+
+        static decimal FactorialDivision(decimal a, decimal b)
+        {
+            return GetFactorial(a) / GetFactorial(b);
+        }
+
+        static void Main()
+        {
+            decimal first = decimal.Parse(Console.ReadLine());
+            decimal second = decimal.Parse(Console.ReadLine());
+            Console.WriteLine($"{FactorialDivision(first, second):f2}");
+        }
+    }
+}
