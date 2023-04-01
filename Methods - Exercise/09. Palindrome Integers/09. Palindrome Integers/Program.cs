a89b415ea66b3a0387f536a24c16a1c6716cad27
KNKostadinov21 <106849849+KNKostadinov21@users.noreply.github.com>
2023-04-01 15:59:36 +0300

    09. Palindrome Integers

---
+++ Methods - Exercise/09. Palindrome Integers/09. Palindrome Integers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _09._Palindrome_Integers
+{
+    internal class Program
+    {
+        static bool DetermineIfPalindrome(string str)
+        {
+            for (int i = 0; i < str.Length / 2; i++)
+                if (str[i] != str[str.Length - 1 - i]) return false;
+
+            return true;
+        }
+
+        static void Main()
+        {
+            string input;
+            while ((input = Console.ReadLine()) != "END")
+                Console.WriteLine(DetermineIfPalindrome(input));
+        }
+    }
+}
