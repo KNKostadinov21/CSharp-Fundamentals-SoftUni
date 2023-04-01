4df80c7994bcb9e941481bd4ba7f4232b6809314
KNKostadinov21 <106849849+KNKostadinov21@users.noreply.github.com>
2023-04-01 18:10:14 +0300

    01. Match Full Name

---
+++ Regular Expressions - Lab/01. Match Full Name/01. Match Full Name/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+internal class Program
+{
+    static void Main()
+    {
+        string regex = @"\b[A-Z][a-z]+ [A-Z][a-z]+\b";
+        string names = Console.ReadLine();
+
+        MatchCollection matchedNames = Regex.Matches(names, regex);
+
+        foreach (Match name in matchedNames)
+            Console.Write(name.Value + " ");
+
+        Console.WriteLine();
+    }
+}
