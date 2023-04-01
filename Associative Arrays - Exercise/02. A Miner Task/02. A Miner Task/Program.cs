d8d5631f98313c3e0e52e3a64d5a017befcbd9f5
KNKostadinov21 <106849849+KNKostadinov21@users.noreply.github.com>
2023-04-01 16:25:16 +0300

    02. A Miner Task

---
+++ Associative Arrays - Exercise/02. A Miner Task/02. A Miner Task/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+internal class Program
+{
+    static void Main()
+    {
+        Dictionary<string, int> oreQuantityPairs = new Dictionary<string, int>();
+        string input;
+        while ((input = Console.ReadLine()) != "stop")
+        {
+            string newOre = input;
+            int newQuantity = int.Parse(Console.ReadLine());
+            if (!oreQuantityPairs.ContainsKey(newOre))
+                oreQuantityPairs.Add(newOre, 0);
+            oreQuantityPairs[newOre] += newQuantity;
+        }
+
+        foreach (var oreQuantity in oreQuantityPairs)
+        {
+            Console.WriteLine($"{oreQuantity.Key} -> {oreQuantity.Value}");
+        }
+    }
+}
