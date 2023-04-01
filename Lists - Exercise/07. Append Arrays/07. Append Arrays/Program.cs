c51307eb8a0684de67c501f4584a7bb53d251e44
KNKostadinov21 <106849849+KNKostadinov21@users.noreply.github.com>
2023-04-01 16:09:53 +0300

    07. Append Arrays

---
+++ Lists - Exercise/07. Append Arrays/07. Append Arrays/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07._Append_Arrays
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string[] inputArray = Console.ReadLine().Split('|').ToArray();
+            List<int> result = new List<int>();
+            for (int i = inputArray.Length - 1; i >= 0; i--)
+            {
+                char[] separators = { ' ' };
+                List<int> trimmedNums = inputArray[i]
+                        .Split(separators, StringSplitOptions.RemoveEmptyEntries)
+                        .Select(int.Parse)
+                        .ToList();
+                result.AddRange(trimmedNums);
+            }
+            Console.WriteLine(string.Join(" ", result));
+        }
+    }
+}
