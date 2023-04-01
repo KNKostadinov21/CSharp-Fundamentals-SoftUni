a94c524a529db63c03151729b186a976d7b24151
KNKostadinov21 <106849849+KNKostadinov21@users.noreply.github.com>
2023-04-01 18:00:05 +0300

    01. Valid Usernames

---
+++ Text Processing - Exercise/01. Valid Usernames/01. Valid Usernames/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+internal class Program
+{
+    static void Main()
+    {
+        string[] inputNames = Console.ReadLine().Split(", ");
+        var validNames = new List<string>();
+        foreach (string name in inputNames)
+        {
+            if (name.Length < 3 || name.Length > 16)
+                continue;
+
+            bool allCharsAreValid = true;
+            foreach (char c in name)
+                if (!IsValidChar(c))
+                {
+                    allCharsAreValid = false;
+                    break;
+                }
+
+            if (allCharsAreValid) validNames.Add(name);
+        }
+        Console.WriteLine(String.Join(Environment.NewLine, validNames));
+    }
+
+    private static bool IsValidChar(char c)
+       => (char.IsLetterOrDigit(c) || c == '-' || c == '_');
+}
