f8dcde3be7df7a4004bd632b4540140be2b94758
KNKostadinov21 <106849849+KNKostadinov21@users.noreply.github.com>
2023-04-01 15:45:06 +0300

    02. Vowels Count

---
+++ Methods - Exercise/02. Vowels Count/02. Vowels Count/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _02._Vowels_Count
+{
+    internal class Program
+    {
+        static void PrintNumberOfVowels(string str)
+        {
+            int vowelCount = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                switch (str[i])
+                {
+                    case 'a':
+                    case 'e':
+                    case 'o':
+                    case 'i':
+                    case 'u': vowelCount++; break;
+                }
+            }
+            Console.WriteLine(vowelCount);
+        }
+
+        static void Main()
+        {
+            string input = Console.ReadLine().ToLower();
+            PrintNumberOfVowels(input);
+        }
+    }
+}
