13c2305f24f07907e45df54814d315a51cb71082
KNKostadinov21 <106849849+KNKostadinov21@users.noreply.github.com>
2023-04-01 15:54:30 +0300

    06. Middle Characters

---
+++ Methods - Exercise/06. Middle Characters/06. Middle Characters/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _06._Middle_Characters
+{
+    internal class Program
+    {
+        static void PrintMiddleChars(string str)
+        {
+            if (str.Length % 2 == 0)
+                Console.WriteLine($"{str[str.Length / 2 - 1]}{str[str.Length / 2]}");
+            else Console.WriteLine(str[str.Length / 2]);
+        }
+
+        static void Main()
+        {
+            string input = Console.ReadLine();
+            PrintMiddleChars(input);
+        }
+    }
+}
