6c8c8bcaf9f3acec3b7bdd2c71b3b889b3bc1e3c
KNKostadinov21 <106849849+KNKostadinov21@users.noreply.github.com>
2023-04-01 16:19:59 +0300

    03. Word Synonyms

---
+++ Associative Arrays - Lab/03. Word Synonyms/03. Word Synonyms/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+internal class Program
+{
+    static void Main()
+    {
+        Dictionary<string, List<string>> words = new Dictionary<string, List<string>>();
+        int countWords = int.Parse(Console.ReadLine());
+        for (int i = 0; i < countWords; i++)
+        {
+            string newWord = Console.ReadLine();
+            string newSynonym = Console.ReadLine();
+            if (!words.ContainsKey(newWord))
+                words.Add(newWord, new List<string>());
+            words[newWord].Add(newSynonym);
+        }
+        foreach (KeyValuePair<string, List<string>> word in words)
+        {
+            Console.WriteLine($"{word.Key} - {String.Join(", ", word.Value)}");
+        }
+    }
+}
