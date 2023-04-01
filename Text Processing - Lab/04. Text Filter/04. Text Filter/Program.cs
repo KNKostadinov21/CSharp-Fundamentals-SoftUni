9ba7509a69d3018d47822282d4c2681fe4276269
KNKostadinov21 <106849849+KNKostadinov21@users.noreply.github.com>
2023-04-01 17:51:01 +0300

    04. Text Filter

---
+++ Text Processing - Lab/04. Text Filter/04. Text Filter/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+
+internal class Program
+{
+    static void Main()
+    {
+        string[] bannedWords = Console.ReadLine().Split(", ");
+        string bigText = Console.ReadLine();
+
+        foreach (string word in bannedWords)
+        {
+            string replacement = new String('*', word.Length);
+
+            while (bigText.Contains(word))
+                bigText = bigText.Replace(word, replacement);
+        }
+
+        Console.WriteLine(bigText);
+    }
+}
