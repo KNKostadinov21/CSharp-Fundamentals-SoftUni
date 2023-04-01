ac0d62fb2eff768887a1181bfa618a1c503a0732
KNKostadinov21 <106849849+KNKostadinov21@users.noreply.github.com>
2023-04-01 18:11:22 +0300

    02. Match Phone Number

---
+++ Regular Expressions - Lab/02. Match Phone Number/02. Match Phone Number/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+internal class Program
+{
+    static void Main()
+    {
+        string regex = @"\+359([ -])2\1\d{3}\1\d{4}\b";
+        string phones = Console.ReadLine();
+
+        MatchCollection phoneMatches = Regex.Matches(phones, regex);
+        List<string> matchedPhones = phoneMatches.Cast<Match>().Select(m => m.Value).ToList();
+
+        Console.Write(String.Join(", ", matchedPhones));
+
+    }
+}
