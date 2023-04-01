9b6ec8a8b2b712d7fe8a994bd8bf5431f1c0fb76
KNKostadinov21 <106849849+KNKostadinov21@users.noreply.github.com>
2023-04-01 15:52:42 +0300

    05. Add and Subtract

---
+++ Methods - Exercise/05. Add and Subtract/05. Add and Subtract/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace _05._Add_and_Subtract
+{
+    internal class Program
+    {
+        static int SumNumbers(int x, int y)
+        {
+            return x + y;
+        }
+
+        static int SubtractNumbers(int x, int y)
+        {
+            return x - y;
+        }
+
+        static void Main()
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            Console.WriteLine(SubtractNumbers(SumNumbers(a, b), c));
+        }
+    }
+}
