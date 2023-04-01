9b2a32ef704f0c77c28b64e7ad66f8be8f4e143f
KNKostadinov21 <106849849+KNKostadinov21@users.noreply.github.com>
2023-04-01 17:47:21 +0300

    03. Substring

---
+++ Text Processing - Lab/03. Substring/03. Substring/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text;
+
+internal class Program
+{
+    static void Main()
+    {
+        string subString = Console.ReadLine();
+        string mainString = Console.ReadLine();
+
+        while (mainString.Contains(subString))
+        {
+            int index = mainString.IndexOf(subString);
+            mainString = mainString.Remove(index, subString.Length);
+        }
+
+        Console.WriteLine(mainString);
+    }
+}
