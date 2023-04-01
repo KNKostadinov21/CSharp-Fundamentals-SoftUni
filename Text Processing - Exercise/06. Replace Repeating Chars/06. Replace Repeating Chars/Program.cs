a73cd3b67b829bd2a91d70ef8c6b36ec14c377d2
KNKostadinov21 <106849849+KNKostadinov21@users.noreply.github.com>
2023-04-01 18:06:40 +0300

    06. Replace Repeating Chars

---
+++ Text Processing - Exercise/06. Replace Repeating Chars/06. Replace Repeating Chars/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+
+internal class Program
+{
+    static void Main()
+    {
+        StringBuilder sb = new StringBuilder();
+        sb.Append(Console.ReadLine());
+        int index = 0;
+
+        while (index < sb.Length - 1)
+        {
+            if (sb[index] == sb[index + 1])
+                sb.Remove(index + 1, 1);
+            else index++;
+        }
+
+        Console.WriteLine(sb);
+    }
+}
