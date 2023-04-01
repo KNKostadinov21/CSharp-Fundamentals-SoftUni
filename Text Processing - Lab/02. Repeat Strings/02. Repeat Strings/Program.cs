45f2bded1a073dafa4af1aa9bff200272279a0b9
KNKostadinov21 <106849849+KNKostadinov21@users.noreply.github.com>
2023-04-01 17:44:39 +0300

    02. Repeat Strings

---
+++ Text Processing - Lab/02. Repeat Strings/02. Repeat Strings/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text;
+
+internal class Program
+{
+    static void Main()
+    {
+        string[] input = Console.ReadLine().Split();
+        StringBuilder result = new StringBuilder();
+
+        for (int wordCount = 0; wordCount < input.Length; wordCount++)
+        {
+            string word = input[wordCount];
+            for (int i = 0; i < word.Length; i++)
+                result.Append(word);
+        }
+        Console.WriteLine(result.ToString());
+    }
+}
