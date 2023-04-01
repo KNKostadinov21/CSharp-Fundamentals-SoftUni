9350835ee685d7f91e1ec8e00fc19e5d12d8c6cf
KNKostadinov21 <106849849+KNKostadinov21@users.noreply.github.com>
2023-04-01 16:04:29 +0300

    03. House Party

---
+++ Lists - Exercise/03. House Party/03. House Party/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace _03._House_Party
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            List<string> list = new List<string>();
+            int numberOfLines = int.Parse(Console.ReadLine());
+            for (int line = 0; line < numberOfLines; line++)
+            {
+                string[] input = Console.ReadLine().Split().ToArray();
+                string name = input[0];
+                if (input[2] == "not") 
+                {
+                    if (list.Contains(name)) list.Remove(name);
+                    else Console.WriteLine($"{name} is not in the list!");
+                }
+                else 
+                {
+                    if (list.Contains(name)) Console.WriteLine($"{name} is already in the list!");
+                    else list.Add(name);
+                }
+            }
+            Console.WriteLine(string.Join("\n", list));
+        }
+    }
+}
