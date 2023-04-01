b051f1e0bbe559bd269d738ab4ba1b57e0d9c7b8
KNKostadinov21 <106849849+KNKostadinov21@users.noreply.github.com>
2023-04-01 16:03:01 +0300

    02.Change List

---
+++ Lists - Exercise/02.Change List/02.Change List/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+class Program
+{
+    static void Main()
+    {
+        List<int> nums = Console.ReadLine().Split().Select(int.Parse).ToList();
+        string input;
+        while ((input = Console.ReadLine()) != "end")
+        {
+            string[] command = input.Split().ToArray();
+            if (command[0] == "Delete") nums.RemoveAll(x => x == int.Parse(command[1]));
+            else if (command[0] == "Insert") nums.Insert(int.Parse(command[2]), int.Parse(command[1]));
+        }
+        Console.WriteLine(string.Join(" ", nums));
+    }
+}
