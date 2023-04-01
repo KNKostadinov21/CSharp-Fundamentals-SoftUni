cb5011cac292d2dea9911644dedccb741533a7a5
KNKostadinov21 <106849849+KNKostadinov21@users.noreply.github.com>
2023-04-01 17:42:23 +0300

    01. Reverse Strings

---
+++ Text Processing - Lab/01. Reverse Strings/01. Reverse Strings/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text;
+
+internal class Program
+{
+    static void Main()
+    {
+        string word;
+        while ((word = Console.ReadLine()) != "end")
+        {
+            StringBuilder reversedWord = new StringBuilder();
+
+            for (int i = word.Length - 1; i >= 0; i--)
+                reversedWord.Append(word[i]);
+
+            Console.WriteLine($"{word} = {reversedWord}");
+        }
+    }
+}
