9020bcd8a47bae26717c9f7decfa243f49c5a7e6
KNKostadinov21 <106849849+KNKostadinov21@users.noreply.github.com>
2023-04-01 17:52:32 +0300

    05. Digits, Letters and Other

---
+++ Text Processing - Lab/05. Digits, Letters and Other/05. Digits, Letters and Other/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+
+internal class Program
+{
+    static void Main()
+    {
+        string inputString = Console.ReadLine();
+        StringBuilder digits = new StringBuilder();
+        StringBuilder letters = new StringBuilder();
+        StringBuilder other = new StringBuilder();
+
+        foreach (char c in inputString)
+        {
+            if (char.IsDigit(c)) digits.Append(c);
+            else if (char.IsLetter(c)) letters.Append(c);
+            else other.Append(c);
+        }
+        Console.WriteLine(digits);
+        Console.WriteLine(letters);
+        Console.WriteLine(other);
+    }
+}
